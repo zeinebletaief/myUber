0b77003e24787364a25d9388da35ac06ab388a5b
zainebLetaief <33903505+zeinebletaief@users.noreply.github.com>
2018-11-11 18:20:40 +0100

    Update UberBlack.java

--- myUber/src/myUberCore/UberBlack.java
+++ myUber/src/myUberCore/UberBlack.java
@@ -45,7 +45,65 @@ public class UberBlack implements Ride{
 			else {basicRate = 2.6;}}
 		}
 		
+		return basicRate * trafficRate * length;package myUberCore;
+
+public class UberBlack implements Ride{
+	double length; //Ride length in km
+	public Driver driver;
+	public Customer customer;
+	static Car car = new Berline();
+	public double[] trafficState = new double[3]; //holds the traffic probability of each state (low, medium & high)
+	
+//constructor 	
+	public UberBlack(Customer customer, double[] trafficState , double length) {
+		super();
+		this.customer = customer;
+		this.length = length;
+		this.trafficState = trafficState; 
+		
+	}
+
+	
+public UberBlack(Customer customer) {
+	super();
+	this.customer = customer;
+}
+
+
+//getters & setters
+	
+	                      
+
+
+
+	
+	//*****************************************************************//
+//	Methods 							   //
+//*****************************************************************//	
+
+	@Override
+	public double rideFare() {
+		double trafficRate = 1 * trafficState[0] + 1.3 * trafficState[1] + 1.6 * trafficState[2];
+		double basicRate = 0;
+		if (length <5) { basicRate = 6.2;}
+		else{
+			if (10 > length)  { basicRate = 5.5;}
+			else {
+				if (length <20) { basicRate = 3.25;}
+			else {basicRate = 2.6;}}
+		}
+		
 		return basicRate * trafficRate * length;
+	}
+
+	@Override
+	public void rideSearch() {
+		// TODO Auto-generated method stub
+		
+	}
+	}
+
+
 	}
 	}
 
