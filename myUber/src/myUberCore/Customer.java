0e4266fa0fbdb2e1200404a341caf241b7373162
zainebLetaief <33903505+zeinebletaief@users.noreply.github.com>
2018-11-11 18:26:36 +0100

    Update Customer.java

--- myUber/src/myUberCore/Customer.java
+++ myUber/src/myUberCore/Customer.java
@@ -45,7 +45,138 @@ public class Customer implements Observer{
 	
 //Getters & setters
 	public long getCounter() {
+		return counter;package myUberCore;
+
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+public class Customer implements Observer{
+	public MyUber myUber; 
+	private static long counter;
+	public String name;
+	public String surname;
+	private Long customerId;
+	public GPS cusPosition;
+	private ArrayList<Message>  messageBox;
+	private CustomerBalance balance;
+	private long creditCard; 
+	private Observable booking;
+	public Ride ongoingRide; //when booking is confirmed 
+	public RideType type; 
+	
+	//constructors
+	public Customer(String name, String surname, long creditCard) {
+		super();
+		this.name = name;
+		this.surname = surname;
+		this.creditCard = creditCard;
+		Customer.counter ++;
+		this.customerId = counter;
+		this.messageBox = new ArrayList<Message>();  
+		this.cusPosition = new GPS(0, 0);
+		this.balance = new CustomerBalance();
+		this.ongoingRide = null; 
+	}
+	public Customer(String name, String surname, long creditCard, GPS point) {
+		super();
+		this.name = name;
+		this.surname = surname;
+		this.creditCard = creditCard;
+		Customer.counter ++;
+		this.customerId = counter;
+		this.messageBox = new ArrayList<Message>();  
+		this.cusPosition = point;
+		this.balance = new CustomerBalance();
+		this.ongoingRide = null; 
+	}
+	
+	
+	
+//Getters & setters
+	public long getCounter() {
 		return counter;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public Long getCustomerId() {
+		return customerId;
+	}
+	public GPS getCusPosition() {
+		return cusPosition;
+	}
+	public ArrayList<Message> getMessageBox() {
+		return messageBox;
+	}
+	public CustomerBalance getBalance() {
+		return balance;
+	}
+	public long getCreditCard() {
+		return creditCard;
+	}
+	public Observable getBooking() {
+		return booking;
+	}
+	
+	public void setBooking(Observable booking) {
+		this.booking = booking;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+	public void setCreditCard(long creditCard) {
+		this.creditCard = creditCard;
+	}
+
+
+	//*****************************************************************//
+//	Methods 							   //
+//*****************************************************************//	
+
+	public void bookRide(GPS destination ) {
+		RideBooking ongoingBooking = new RideBooking(this, destination, this.myUber);
+		this.setBooking(ongoingBooking);
+	}
+	
+	public void bookRide(GPS start, GPS destination ) {
+		RideBooking ongoingBooking = new RideBooking(this, start, destination, this.myUber);
+		this.setBooking(ongoingBooking);
+		//i.e creation RideBooking it is an observable (so this is an addObservable Method)		
+		}
+	
+	
+	
+	public void chooseRideType(String type) {
+		((RideBooking) this.booking).setRidePreference(type);
+		((RideBooking) this.booking).carSearch();
+	}
+	
+	
+	public void evaluate(Ride ride) {
+		//TODO
+		//on termination of the ride , so must know when it ends and compare with current time
+		
+	}
+	
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
 	}
 	public String getName() {
 		return name;
